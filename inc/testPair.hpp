e1067a5c4ad54c392ad8f8356bce96b258218c40
Eduardo_AG <hectoreduardo.alcantargutierrez@bosch.com>
2025-09-10 09:15:37 -0600

    Modularity

---
+++ inc/testPair.hpp
@@ -0,0 +1,11 @@
+#include "colorPair.hpp"
+
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor);
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber);
